14149cff39f9255c08bd50112ec51c724daa38cc
root <andrea.munozgr@gmail.com>
2025-08-19 00:36:37 +0000

    Adding shell.c

---
+++ shell.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
+/**
+ * main - first version of a super simple shell that can
+ * run commands with their full path, without any argument.
+ * line: pointer where the line will be store in the buffer.
+ * size: pointer to the size of the buffer.
+ * stdin: get the line from the user typing.
+ *
+ * Return: Always 0.
+ */
+void handled_sigint(int sig)
+{
+	(void)sig;
+	exit(0);
+}
+
+int main(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t read;
+	int status, cont_argv;
+	char *argv[3];
+	char *token;
+	pid_t child_pid;
+
+	signal(SIGINT, handled_sigint);
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("#cisfun$");
+		}
+		read = getline(&line, &size, stdin);
+		if (read == -1)
+		{
+			free(line);
+			exit(0);
+		}
+		line[strcspn(line, "\n")] = '\0';
+		token = strtok(line, " ");
+		cont_argv = 0;
+		while (token != NULL && cont_argv < 3)
+		{
+			argv[cont_argv] = token;
+			cont_argv++;
+			token = strtok(NULL, " ");
+		}
+		argv[cont_argv] = NULL;
+
+		child_pid = fork();
+		if (child_pid == -1)
+	    	{
+	    		perror("Error on the fork");
+		    	return (1);
+	    	}
+		if (child_pid == 0)
+	    	{
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror("./shell");
+			}
+		}
+		else 
+		{
+			wait(&status);
+		}
+
+	}
+	free(line);
+	return (0); 
+}
